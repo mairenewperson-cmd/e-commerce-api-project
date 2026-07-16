dcf6eefbe2660fb69b92ffff1865c66489de5d53
mairenewperson-cmd <mairenewperson@gmail.com>
2026-07-16 20:16:14 +0300

    feat: add cartroute

--- routes/cartroute.js
+++ routes/cartroute.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const cartController = require('../controllers/cartcontroller'); // Adjust path
-
 const {
-getCart,
-addToCart,
-updateCart,
-removeFromCart,
-clearCart
-} = require(" .. /controllers/cartcontroller");
-const cartValidator = require(" .. /controllers/cartcontroller");
-const validatorMiddleware = require(" .. /middleware/errorhandler");
+  getCart,
+  addItemToCart,
+  updateItemQuantity,
+  removeItemFromCart,
+  clearCart,
+} = require('../controllers/cartcontroller');
+
 router.route('/').get(getCart).delete(clearCart);
 router.route('/items').post(addItemToCart);
 router.route('/items/:productId').patch(updateItemQuantity).delete(removeItemFromCart);
-
-
-// Define the POST endpoint for adding items
-router.post('/add', cartcontroller.addItemToCart);
 
 module.exports = router;
