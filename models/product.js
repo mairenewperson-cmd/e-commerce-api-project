3bd05d469c2ef838971c75ff19ff5776b33bcb79
mairenewperson-cmd <mairenewperson@gmail.com>
2026-07-18 19:38:14 +0300

    Update product.js

--- models/product.js
+++ models/product.js
@@ -6,13 +6,29 @@ const productSchema = new mongoose.Schema({
     required: [true, 'Please add a product name'],
     trim: true
   },
+  description: {
+    type: String,
+    required: [true, 'Please add a product description'],
+    trim: true
+  },
   price: {
     type: Number,
-    required: [true, 'Please add a product price']
+    required: [true, 'Please add a product price'],
+    min: [0, 'Price cannot be less than 0']
+  },
+  stock: {
+    type: Number,
+    min: [0, 'Stock cannot be less than 0'],
+    default: 0
   },
   category: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category', 
     required: [true, 'Please add a product category']
+  },
+  images: {
+    type: [String],
+    default: [] 
   },
   inStock: {
     type: Boolean,
@@ -20,6 +36,13 @@ const productSchema = new mongoose.Schema({
   }
 }, {
   timestamps: true
+});
+
+
+productSchema.pre('save', function() {
+
+  const currentStock = this.stock ?? 0;
+  this.inStock = currentStock > 0;
 });
 
 module.exports = mongoose.model('Product', productSchema);
